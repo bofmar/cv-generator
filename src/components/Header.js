71cf4e051d783b2ed944a5830cd1d7a3b7b2aff8
bofmar <bofmar@outlook.com>
2022-11-22 12:43:54 +0200

    Add logo

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from '../images/cv-generator-logo.png';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} className='logo' />
+    </header>
+  );
+}
