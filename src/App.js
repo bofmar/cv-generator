d67f9a6a4d367bc80a6c27ee935a27c3b7948f82
bofmar <bofmar@outlook.com>
2022-11-23 12:45:41 +0200

    Remove unused inport

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 import inputs, { input } from './helpers/inputs';
