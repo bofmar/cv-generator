215869b9edf44413a8b952f79302087095be000e
bofmar <bofmar@outlook.com>
2022-11-23 13:29:38 +0200

    Add contact and experience sections to cv

--- src/components/CV.js
+++ src/components/CV.js
@@ -9,6 +9,25 @@ export default function CV({ personalInfo, description, experience, education, s
         <h1 className='full-name'>{fullName}</h1>
         <h2 className='title'>{title.value}</h2>
       </section>
+      <section className='contacts'>
+        <h1>CONTACT INFOR</h1>
+        <p className='phone-number'>{phoneNumber.value}</p>
+        <p className='address'>{address.value}</p>
+        <p className='email'>{email.value}</p>
+      </section>
+      <section className='description'>
+        <h1>ABOUT ME</h1>
+        <p>{description}</p>
+      </section>
+      <section className='experience'>
+        <h1>EXPERIENCE</h1>
+        {experience.map(exp => (
+          <div className='experience-wrapper'>
+            <h1>{exp[1][0].value} - {exp[1][1].value}</h1>
+            <p>{exp[1][2].value} - {exp[1][3].value}</p>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
